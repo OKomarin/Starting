07f0853161f1c5981438532da95c092478db4dab
OKomarin <olga.johanson@gmail.com>
2022-07-28 23:34:09 +0300

    Last and done version of first code.

--- src/TransitCalculator.java
+++ src/TransitCalculator.java
@@ -1,19 +1,16 @@
 public class TransitCalculator {
-
     String[] fareOptions = {"Pay-per-ride (single ride)","7-day Unlimited Rides","30-day Unlimited Rides"};
     double[] fares = {2.75,33,127};
-    int days;
+    double days;
     int rides;
-
-
-    public TransitCalculator(int numRides, int numDays) {
+    public TransitCalculator(int numRides, double numDays) {
         rides = numRides;
         days = numDays;
     }
-
     public double unlimited7Price() {
         /* Calculating price per ride for 7 day unlimited pass */
-        double sevenDayPassNeeded = Math.ceil(days / 7.0);
+        double sevenDayPassNeeded = Math.ceil(days / 7);
+        //if days int - double sevenDayPassNeeded = Math.ceil(days / 7.0);- better
         double pricePerRide7D = sevenDayPassNeeded * fares[1];
         return pricePerRide7D / rides;
     }
@@ -43,22 +40,12 @@ public class TransitCalculator {
         TransitCalculator test4 = new TransitCalculator(54,26);
         TransitCalculator test5 = new TransitCalculator(12,6);
         //Testing values to check work of unlimited7Price
-        System.out.println("test1 " + test1.unlimited7Price());
-        System.out.println("test2 " + test2.unlimited7Price());
-        System.out.println("test3 " + test3.unlimited7Price());
+        System.out.println("Test1: " + test1.unlimited7Price());
+        System.out.println("Test2: " + test2.unlimited7Price());
+        System.out.println("Test3: " + test3.unlimited7Price());
        // Best price value test
-        System.out.println("test4 " + test4.getBestFare());
-        System.out.println("test5 " + test5.getBestFare());
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Test4: " + test4.getBestFare());
+        System.out.println("Test5: " + test5.getBestFare());
 
     }
 
