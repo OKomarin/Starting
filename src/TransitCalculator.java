b1e5f2f65841dfbc30973c45dc2df7b29c3043a8
OKomarin <olga.johanson@gmail.com>
2022-07-28 21:55:57 +0300

    Last and done version of first code.

---
+++ src/TransitCalculator.java
@@ -0,0 +1,66 @@
+public class TransitCalculator {
+
+    String[] fareOptions = {"Pay-per-ride (single ride)","7-day Unlimited Rides","30-day Unlimited Rides"};
+    double[] fares = {2.75,33,127};
+    int days;
+    int rides;
+
+
+    public TransitCalculator(int numRides, int numDays) {
+        rides = numRides;
+        days = numDays;
+    }
+
+    public double unlimited7Price() {
+        /* Calculating price per ride for 7 day unlimited pass */
+        double sevenDayPassNeeded = Math.ceil(days / 7.0);
+        double pricePerRide7D = sevenDayPassNeeded * fares[1];
+        return pricePerRide7D / rides;
+    }
+    public double[] getRidePrices(){
+        /* Calculating price per trip*/
+        double singleTicket = fares[0];
+        double monthlyPass = fares[2]/rides;
+        return new double[]{singleTicket,unlimited7Price(), monthlyPass};
+    }
+    public String getBestFare(){
+        double[] ridePrice = getRidePrices();
+        int bestPriceIndex = 0;
+
+        for (int i = 0; i < ridePrice.length; i++){
+            if (ridePrice[i] < ridePrice[bestPriceIndex]){
+                bestPriceIndex = i;
+            }
+        }
+        return "You should get the " + fareOptions[bestPriceIndex] + " option at " + Math.round(ridePrice[bestPriceIndex]*100.0) / 100.0 + " per ride.";
+
+    }
+    public static void main(String[] args){
+
+        TransitCalculator test1 = new TransitCalculator(20,19);
+        TransitCalculator test2 = new TransitCalculator(50,22);
+        TransitCalculator test3 = new TransitCalculator(14,6);
+        TransitCalculator test4 = new TransitCalculator(54,26);
+        TransitCalculator test5 = new TransitCalculator(12,6);
+        //Testing values to check work of unlimited7Price
+        System.out.println("test1 " + test1.unlimited7Price());
+        System.out.println("test2 " + test2.unlimited7Price());
+        System.out.println("test3 " + test3.unlimited7Price());
+       // Best price value test
+        System.out.println("test4 " + test4.getBestFare());
+        System.out.println("test5 " + test5.getBestFare());
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
